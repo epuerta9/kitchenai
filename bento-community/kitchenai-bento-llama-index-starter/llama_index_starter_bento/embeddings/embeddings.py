587447b87ca4e68f3190fbf86213f8ade8ae611a
epuerta <esteban_puerta@rhinosearch.io>
2024-12-26 17:06:57 -0600

    wip: plugin deepeval

--- bento-community/kitchenai-bento-llama-index-starter/llama_index_starter_bento/embeddings/embeddings.py
+++ bento-community/kitchenai-bento-llama-index-starter/llama_index_starter_bento/embeddings/embeddings.py
@@ -1,5 +1,5 @@
 from llama_index_starter_bento.kitchen import app as kitchen
-from kitchenai.contrib.kitchenai_sdk.api import EmbedSchema
+from kitchenai.contrib.kitchenai_sdk.schema import EmbedSchema
 import logging
 
 logger = logging.getLogger(__name__)
