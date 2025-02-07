1d7857ecd218e2856bd275d898d549b7a266332a
epuerta <esteban_puerta@rhinosearch.io>
2025-02-07 01:57:09 -0600

    removed apps community. not needed atm

--- apps-community/kitchenai-app-kitchenai-playground/kitchenai_playground_app/signals.py
+++
@@ -1,5 +0,0 @@
-
-
-# Django signals allow plugins to hook into and react to model changes and other events.
-# Common signals include pre_save, post_save, pre_delete, and post_delete.
-# Plugins can connect signal handlers to perform custom logic when these events occur.
--- apps-community/kitchenai-app-kitchenai-playground/kitchenai_playground_app/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2024-present epuerta <esteban_puerta@rhinosearch.io>
-#
-# SPDX-License-Identifier: MIT
