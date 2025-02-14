bcb0e7d7003c6798c105db541dc1af2ace9cb243
epuerta <epuer94@gmail.com>
2025-02-13 21:51:51 -0600

    test requirements

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-from typer.testing import CliRunner
-from whisk.whisk.cli import app
-
-runner = CliRunner()
-
-def test_version():
-    result = runner.invoke(app, ["--version"])
-    assert result.exit_code == 0
-    assert "KitchenAI Whisk v" in result.stdout 
